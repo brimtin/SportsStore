755592df167eb3753694fe3613595f008b51801b
Ibrahim <ibrahim.toure2019@outlook.com>
2019-09-18 08:15:22 -0400

    Created a Sports Store

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Component
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the nav view Component";
+        }
+    }
+}
